d7634694254c965b5bd43562df9d030454e9799a
RapTho <raphael.tholl@gmail.com>
2021-06-12 00:13:20 +0200

    export connect function

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
 const chalk = require("chalk");
 
-mongoose
-  .connect(process.env.MONGODBURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then(() => console.log("MongoDB connected!"))
-  .catch((err) => {
-    throw new Error(chalk.bold.red(`MongoDB error: ${err.message}`));
-  });
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGODBURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    .then(() => console.log("MongoDB connected!"))
+    .catch((err) => {
+      throw new Error(chalk.bold.red(`MongoDB error: ${err.message}`));
+    });
+};
+
+module.exports = { connect };
